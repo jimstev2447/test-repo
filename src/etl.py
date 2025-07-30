28bb52f58e3d03b4eda4eae26cb75ffb37da4031
jimstev2447 <jim.stevenson2447@gmail.com>
2025-07-30 10:51:16 +0100

    edit to etl

--- src/etl.py
+++ src/etl.py
@@ -8,7 +8,9 @@ def lambda_handler(event, context):
     STORAGE_BUCKET_NAME = os.environ['STORAGE_BUCKET_NAME']
     s3_bucket_name = event['Records'][0]["s3"]["bucket"]["name"]
     s3_object_name = event['Records'][0]["s3"]["object"]["key"]
+
     file_title = s3_object_name.replace(".txt", ".json")
+
     s3 = boto3.client("s3")
     data = s3.get_object(Bucket=s3_bucket_name, Key=s3_object_name)
 
@@ -16,6 +18,7 @@ def lambda_handler(event, context):
     csv_items = list(csv.reader(contents.splitlines(), delimiter=","))
     headers = csv_items.pop(0)
     teams = Teams(People=People)
+    
     for line in csv_items:
         teams.update(line=line, headers=headers)
 
